3b96ae18c0de2a3fe1389a3b6c705d5d7d01de07
Ishak26 <izhakmahyuddin@gmail.com>
2025-01-01 19:58:32 +0800

    belajar build

--- src/assets/about.jsx
+++ src/assets/about.jsx
@@ -1,9 +1,9 @@
 
 export default function About(){
   return(
-    <div className="max-w-xl min-h-screen flex flex-col justify-center m-auto p-4 ">
-      <span className="font-bold text-2xl text-bluelight">Hei aku Ishak 😎,</span>
-      <p className="text-justify text-base font-sm md:w-1/2">
+    <div className="max-w-xl min-h-screen flex flex-col justify-center items-center">
+      <p className="text-justify text-base font-sm w-1/2">
+      <span className="font-bold text-2xl block text-bluelight">Hei aku Ishak 😎,</span>
         saya seorang <i className="text-neonpink">Web Developer</i>, Siap membantu anda dalam membangun aplikasi berbasis web.
       </p>
     </div>
--- src/assets/learn.jsx
+++ src/assets/learn.jsx
@@ -5,7 +5,7 @@ function ListEducation({educationName,yearStar,yearEnd}){
   return (
     <>
       <li className="p-5 " >
-          <p className="font-semibold" >{educationName} ({yearStar} - {yearEnd})</p>
+          <p className="font-semibold text-sm md:text-base " >{educationName} ({yearStar} - {yearEnd})</p>
           <a className=" font-light border-b-2 border-black hover:border-b-4" href="">Visit More </a>
         </li>
     </>
@@ -15,25 +15,22 @@ function ListEducation({educationName,yearStar,yearEnd}){
 
 function EducationContainer({Education}){
   const changeEducation = () =>{
-    return (Education == "formal")?(
-      <>
-        <ul className="flex m-auto border-l-2 border-black flex-col w-2/4 justify-center mt-14 before:content-[''] before:w-4 before:h-4 before:rounded-full before:bg-black before:mt-[-0.5rem] before:ms-[-0.5rem] after:content-[''] after:w-4 after:h-4 after:rounded-full after:bg-black after:mb-[-0.5rem] after:ms-[-0.5rem]">
-            <ListEducation educationName={"Sman 9 Tana Toraja"} yearStar={"2016"} yearEnd={"2019"} />
-            <ListEducation educationName={"Universitas Dipa Makassar"} yearStar={"2020"} yearEnd={"2024"} />      
+    return(
+        <ul className="flex m-auto border-l-2 border-black flex-col w-2/4 justify-center my-8 before:content-[''] before:w-4 before:h-4 before:rounded-full before:bg-black before:mt-[-0.5rem] before:ms-[-0.5rem] after:content-[''] after:w-4 after:h-4 after:rounded-full after:bg-black after:mb-[-0.5rem] after:ms-[-0.5rem]">
+          {(Education == "formal")? 
+            <>
+              <ListEducation educationName={"Sman 9 Tana Toraja"} yearStar={"2016"} yearEnd={"2019"} />
+              <ListEducation educationName={"Universitas Dipa Makassar"} yearStar={"2020"} yearEnd={"2024"} />
+            </>
+            :
+            <>
+              <ListEducation educationName={"Operator Alat Berat"} yearStar={"Maret 2023"} yearEnd={"Maret 2023"} />
+              <ListEducation educationName={"Junior Network"} yearStar={"Maret 2023"} yearEnd={"Maret 2023"} />
+              <ListEducation educationName={"Creating 3D Asset"} yearStar={"2020"} yearEnd={"2024"} />
+            </>     
+          }
         </ul>  
-      </>
-    )
-    :
-    (
-      <>
-        <ul className="flex m-auto border-l-2 border-black flex-col w-2/4 justify-center mt-14 before:content-[''] before:w-4 before:h-4 before:rounded-full before:bg-black before:mt-[-0.5rem] before:ms-[-0.5rem] after:content-[''] after:w-4 after:h-4 after:rounded-full after:bg-black after:mb-[-0.5rem] after:ms-[-0.5rem]">
-            <ListEducation educationName={"Operator Alat Berat"} yearStar={"Maret 2023"} yearEnd={"Maret 2023"} />
-            <ListEducation educationName={"Junior Network"} yearStar={"Maret 2023"} yearEnd={"Maret 2023"} />
-            <ListEducation educationName={"Creating 3D Asset"} yearStar={"2020"} yearEnd={"2024"} />      
-        </ul>  
-      </>
-
-    )
+      )
   }
   return changeEducation()
 
@@ -45,25 +42,22 @@ export default function Learn(){
   const { height } = containerRef
   
   return <div>
-    <section className="mt-14 max-w-screen-md min-h-screen m-auto"> 
+    <section className="max-w-screen-md min-h-screen m-auto"> 
       <p className="text-5xl font-extrabold text-bluelight text-end"> Education </p> 
       <div className="flex flex-column mb-[-2px]">
         <button 
         onClick={()=>{
           setEducation("formal")
         }} 
-        className="px-5 font-mono bg-bluelight border-2 border-black border-b-0 z-20 rounded-tl-xl">Formal</button>
+        className={`px-5 text-black font-mono bg-bluelight border-2 border-black border-r-0 border-b-0 z-20 rounded-tl-xl ${!(Education == "formal") && "text-opacity-50"}`}>Formal</button>
         <button
         onClick={()=>{
           setEducation("Non-Formal")
         }}
-        className="p-3 font-mono bg-neonpink border-2 border-black border-b-0 z-20 rounded-tr-xl">Non-Formal</button>
+        className={`p-3 font-mono bg-neonpink border-2 border-black border-l-0 border-b-0 z-20 rounded-tr-xl text-black ${!(Education=="Non-Formal") && "text-opacity-50"}`}>Non-Formal</button>
       </div>
-
       <motion.div 
-      className={`p-3 rounded-b-xl backdrop-blur-sm bg-opacity-50 border-black border-t-2 border-l-2 border-b-[8px] border-r-[8px] rounded-tr-xl ${(Education != 'formal')?'bg-neonpink':'bg-bluelight'}`}
-      
-      >
+      className={`p-3 rounded-b-xl backdrop-blur-sm bg-opacity-50 border-black border-t-2 border-l-2 border-b-[8px] border-r-[8px] rounded-tr-xl ${(Education != 'formal')?'bg-neonpink':'bg-bluelight'}`}  >
         <EducationContainer Education={Education} />
       </motion.div>
     </section> 
